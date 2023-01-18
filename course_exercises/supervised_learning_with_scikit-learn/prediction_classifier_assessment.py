5d927a23b74a50e1bc1280a46847d8bf37b0b334
Sudip Bhandari <sudipbhandari@Sudips-MacBook.local>
2023-01-17 21:08:26 -0500

    prediction classifier for diabetes df

---
+++ course_exercises/supervised_learning_with_scikit-learn/prediction_classifier_assessment.py
@@ -0,0 +1,24 @@
+# Import confusion matrix
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Create X and y arrays
+X = np.array(diabetes_df['diabetes'])
+y = np.array(diabetes_df[['bmi', 'age']])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
+
+# Fit the model to the training data
+knn.fit(X_train, y_train)
+
+# Predict the labels of the test data: y_pred
+y_pred = knn.predict(X_test)
+
+# Generate the confusion matrix and classification report
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
