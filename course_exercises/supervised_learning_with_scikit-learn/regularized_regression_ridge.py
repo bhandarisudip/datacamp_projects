e579e380d7a39a6e1fd22dc67618cca96c04674f
Sudip Bhandari <sudipbhandari@Sudips-MacBook.local>
2023-01-27 09:59:06 -0500

    update formatting and names for py files

--- course_exercises/supervised_learning_with_scikit-learn/regularized_regression_ridge.py
+++ course_exercises/supervised_learning_with_scikit-learn/regularized_regression_ridge.py
@@ -1,3 +1,8 @@
+"""
+Fit ridge regression models over a range of different alpha values, and 
+print their scores.
+"""
+
 # Import Ridge
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
@@ -9,7 +14,6 @@ y = sales_df["sales"].values
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
-
 
 alphas = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
 ridge_scores = []
